2c2c200ddda7137bbbbb35d489a2dde03ceed9fa
talha-ash <ta.xit@outlook.com>
2025-10-06 12:30:02 +0500

    implement page data preload

--- react-app/scripts/preload-assets.js
+++ react-app/scripts/preload-assets.js
@@ -1,3 +1,24 @@
+const preloadResponses = {}
+
+const originalFetch = window.fetch
+
+window.fetch = async (input, options) => {
+    const requestID = `${input.toString()}${options?.body?.toString() || ''}`
+    const preloadResponse = preloadResponses[requestID]
+
+    if (preloadResponse) {
+        if (!options?.preload) delete preloadResponses[requestID]
+
+        return preloadResponse
+    }
+
+    const response = originalFetch(input, options)
+
+    if (options?.preload) preloadResponses[requestID] = response
+
+    return response
+}
+
 const getPathname = () => {
     let { pathname } = window.location
 
@@ -39,12 +60,40 @@ const preloadScript = (scripts) => {
     document.head.append(...children);
 };
 
+const preloadData = ({ pathname = getPathname(), path, data }) => {
+    data.forEach(({ url, preconnect, ...request }) => {
+        if (url.startsWith('func:')) url = eval(url.replace('func:', ''))
+
+        const constructedURL = typeof url === 'string' ? url : url(getDynamicProperties(pathname, path))
+
+        fetch(constructedURL, { ...request, preload: true })
+
+        // https://issues.chromium.org/issues/380896837
+        preconnect?.forEach(url => {
+            document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'preconnect', href: url }))
+        })
+    })
+}
+
+const getDynamicProperties = (pathname, path) => {
+    const pathParts = path.split('/')
+    const pathnameParts = pathname.split('/')
+    const dynamicProperties = {}
+
+    for (let i = 0; i < pathParts.length; i++) {
+        if (pathParts[i].startsWith(':')) dynamicProperties[pathParts[i].slice(1)] = pathnameParts[i]
+    }
+
+    return dynamicProperties
+}
+
 const currentPage = getPage()
 
 if (currentPage) {
-    const { path, title, scripts } = currentPage
+    const { path, title, scripts, data } = currentPage
 
     preloadScript(scripts)
+    if (data) preloadData({ path, data })
 
     if (title) document.title = title
 }
--- react-app/src/pages.js
+++ react-app/src/pages.js
@@ -2,7 +2,16 @@ export default {
     home: {
         path: '/',
         title: 'Home',
-        description: 'Demonstrated here is a simple page which all of its assets are fetched in parallel.'
+        description: 'Demonstrated here is a simple page which all of its assets are fetched in parallel.',
+        data: [
+            {
+                url: "https://jsonplaceholder.typicode.com/todos",
+                method: 'get',
+                headers: { 'Content-Type': 'application/json' },
+                preconnect: ['https://raw.githubusercontent.com']
+            },
+
+        ]
     },
     about: {
         path: '/about',
